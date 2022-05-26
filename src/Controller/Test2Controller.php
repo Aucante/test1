1e6ec401ac49caab229e9fddf05113629b69d6dd
Aucante <sebastien.aucante@gmail.com>
2022-05-27 00:25:00 +0200

    second controller

---
+++ src/Controller/Test2Controller.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\Routing\Annotation\Route;
+
+class Test2Controller extends AbstractController
+{
+    /**
+     * @Route("/test2", name="app_test2")
+     */
+    public function index(): JsonResponse
+    {
+        return $this->json([
+            'message' => 'Welcome to your new controller!',
+            'path' => 'src/Controller/Test2Controller.php',
+        ]);
+    }
+}
