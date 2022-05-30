88a0a9efc1cca6eed1763bfaa6d6eaf8e8d83759
Aucante <sebastien.aucante@gmail.com>
2022-05-30 23:06:18 +0200

    test6

---
+++ src/Controller/Test6Controller.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\Routing\Annotation\Route;
+
+class Test6Controller extends AbstractController
+{
+    /**
+     * @Route("/test6", name="app_test6")
+     */
+    public function index(): JsonResponse
+    {
+        return $this->json([
+            'message' => 'Welcome to your new controller!',
+            'path' => 'src/Controller/Test6Controller.php',
+        ]);
+    }
+}
