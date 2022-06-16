a0b75e1f8e094753b1f87680ec578255774a4c2a
Aucante <sebastien.aucante@gmail.com>
2022-06-16 21:02:05 +0200

    make controller 7

---
+++ src/Controller/Test7Controller.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\Routing\Annotation\Route;
+
+class Test7Controller extends AbstractController
+{
+    /**
+     * @Route("/test7", name="app_test7")
+     */
+    public function index(): JsonResponse
+    {
+        return $this->json([
+            'message' => 'Welcome to your new controller!',
+            'path' => 'src/Controller/Test7Controller.php',
+        ]);
+    }
+}
