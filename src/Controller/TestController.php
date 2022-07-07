f64a005cb0067f56f79d5a8e9699dcbe242e3bfc
Aucante <sebastien.aucante@gmail.com>
2022-07-07 23:44:38 +0200

    upadte controller

--- src/Controller/TestController.php
+++ src/Controller/TestController.php
@@ -13,6 +13,7 @@ class TestController extends AbstractController
      */
     public function index(): JsonResponse
     {
+        
         return $this->json([
             'message' => 'Welcome to your new controller!',
             'path' => 'src/Controller/TestController.php',
