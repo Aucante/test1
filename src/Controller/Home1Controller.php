ceeb4ac461dacd31ebc753f7691d2137ed437c37
Aucante <sebastien.aucante@gmail.com>
2022-07-26 23:36:50 +0200

    c home1controller

---
+++ src/Controller/Home1Controller.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\Routing\Annotation\Route;
+
+class Home1Controller extends AbstractController
+{
+    /**
+     * @Route("/home1", name="app_home1")
+     */
+    public function index(): JsonResponse
+    {
+        return $this->json([
+            'message' => 'Welcome to your new controller!',
+            'path' => 'src/Controller/Home1Controller.php',
+        ]);
+    }
+}
